421cf599f0af82bf78843588a08ca1ef2233e52b
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-02-28 08:03:56 +0700

    Improving the MealDetails

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
-import 'package:meals_app/screens/meal_detail.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
