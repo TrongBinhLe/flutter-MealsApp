922a5ed7347c22c0beefb2606d5a7c5dd32607c6
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-03-02 07:30:33 +0700

    Appling the filter

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,12 +9,14 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
     super.key,
     required this.onToggleFavorite,
+    required this.availableMeals,
   });
 
   final Function(Meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
