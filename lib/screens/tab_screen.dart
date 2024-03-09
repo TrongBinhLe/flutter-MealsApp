73b42b08d22b449b6665bf6e106d2b1706994bed
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-03-09 18:07:04 +0700

    Connect mutiple provider with each other

--- lib/screens/tab_screen.dart
+++ lib/screens/tab_screen.dart
@@ -6,7 +6,6 @@ import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/filters_screen.dart';
 import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
-import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/providers/filters_provider.dart';
 
 const kInitialFilters = {
@@ -44,24 +43,7 @@ class _TabScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
-
+    final availableMeals = ref.watch(mealFilteredProvider);
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
     );
